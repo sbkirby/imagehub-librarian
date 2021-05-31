6887f3105c91f1d025f5579d23c8d83216219ca8
sbkirby <taghorn@gmail.com>
2021-05-31 09:58:57 -0500

    Update openalpr_script.sh

--- openalpr_script.sh
+++ openalpr_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start python scripts concurrently at docker opencv startup
-cd /home/your_username/IOTstack
+cd /home/YOUR_HOME_DIRECTORY/IOTstack
 # Start MQTT Client script
 python3 mqtt_client.py &
 # Start imagehub with path to imagehub.yaml
-python3 /home/your_username/IOTstack/imagehub/imagehub/imagehub.py -p /home/your_username/IOTstack/imagehub
+python3 /home/YOUR_HOME_DIRECTORY/IOTstack/imagehub/imagehub/imagehub.py -p /home/YOUR_HOME_DIRECTORY/IOTstack/imagehub
