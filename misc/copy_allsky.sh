a2dc9f74cb8c4a629e7ee0467f7ddcbc77731a3e
sbkirby <taghorn@gmail.com>
2021-05-31 10:00:32 -0500

    Update copy_allsky.sh

--- misc/copy_allsky.sh
+++ misc/copy_allsky.sh
@@ -2,7 +2,7 @@
 # copy_allsky.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 cd /home/ftpuser/
-sudo cp -up /home/ftpuser/allsky/keograms/*.* /home/your_username/volumes/nodered/data/imagehub_data/ftp/allsky/keograms/
-sudo cp -up /home/ftpuser/allsky/startrails/*.* /home/your_username/volumes/nodered/data/imagehub_data/ftp/allsky/startrails/
-sudo cp -up /home/ftpuser/allsky/videos/*.* /home/your_usernamea/volumes/nodered/data/imagehub_data/ftp/allsky/videos/
+sudo cp -up /home/ftpuser/allsky/keograms/*.* /home/YOUR_HOME_DIRECTORY/volumes/nodered/data/imagehub_data/ftp/allsky/keograms/
+sudo cp -up /home/ftpuser/allsky/startrails/*.* /home/YOUR_HOME_DIRECTORY/volumes/nodered/data/imagehub_data/ftp/allsky/startrails/
+sudo cp -up /home/ftpuser/allsky/videos/*.* /home/YOUR_HOME_DIRECTORY/volumes/nodered/data/imagehub_data/ftp/allsky/videos/
 cd /
